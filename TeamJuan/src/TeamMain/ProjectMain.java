a83f50e0fa9ee20a74761d1dbceef01213028042
dhillp <dhillp@uw.edu>
2017-01-19 17:46:35 -0800

    Added main team package.

---
+++ TeamJuan/src/TeamMain/ProjectMain.java
@@ -0,0 +1,15 @@
+package TeamMain;
+import java.awt.EventQueue;
+
+public class ProjectMain {
+
+	public static void main(String[] thAargs) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new MainWindow();
+			}
+		});
+	}
+
+}
