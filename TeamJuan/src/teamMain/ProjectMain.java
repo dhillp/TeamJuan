8e29be3abec1bc0b1703349bd623db052ea478fc
dhillp <dhillp@uw.edu>
2017-02-09 16:25:44 -0800

    PSD

--- TeamJuan/src/teamMain/ProjectMain.java
+++ TeamJuan/src/teamMain/ProjectMain.java
@@ -3,7 +3,7 @@ import java.awt.EventQueue;
 
 public class ProjectMain {
 
-	public static void main(String[] theAargs) {
+	public static void main(String[] theArgs) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				new MainWindow();
