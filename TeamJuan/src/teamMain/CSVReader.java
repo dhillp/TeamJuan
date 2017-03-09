5ea0730b72e64938d346cee8f30098958e4b3d72
harman <harman@d-10-16-7-91.dhcp4.washington.edu>
2017-03-09 15:51:58 -0800

    hs

--- TeamJuan/src/teamMain/CSVReader.java
+++ TeamJuan/src/teamMain/CSVReader.java
@@ -9,36 +9,37 @@ public class CSVReader {
 
     public static void main(String[] args) {
 
-        String csvFile = "/Users/mkyong/csv/country.csv";
-        BufferedReader br = null;
-        String line = "";
-        String cvsSplitBy = ",";
-
-        try {
-
-            br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
-
-                // use comma as separator
-                String[] country = line.split(cvsSplitBy);
-
-                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
-
-            }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-    }
+//        String csvFile = "/Users/mkyong/csv/country.csv";
+//        BufferedReader br = null;
+//        String line = "";
+//        String cvsSplitBy = ",";
+//
+//        try {
+//
+//            br = new BufferedReader(new FileReader(csvFile));
+//            while ((line = br.readLine()) != null) {
+//
+//                // use comma as separator
+//                String[] country = line.split(cvsSplitBy);
+//
+//                System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
+//
+//            }
+//
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (br != null) {
+//                try {
+//                    br.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//
+//    }
+}
 }
--- TeamJuan/src/teamMain/SystemRegistration.java
+++ TeamJuan/src/teamMain/SystemRegistration.java
@@ -1,24 +1,78 @@
 package teamMain;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class SystemRegistration {
 	
-	public SystemRegistration(){
-		
-	}
+	private String myFirst;
+	private String myLast;
+	private String myId;
+	private String myEmail;
+	private String myCategory;
 	
-	private void readFile(){
+	public SystemRegistration(String first, String last, String id
+			,String email, String category){
+		myFirst = first;
+		myLast = last;
+		myId = id;
+		myEmail = email;
+		myCategory = category;
 		
 	}
+    public static void main(String [] args) {
+    	
+    }
 	
 	private void writeFile(){
+		String st = toString();
+
+        String f = "Data.txt";
+        String line = null;
+
+        try {
+            FileReader fileReader = 
+                new FileReader(f);
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }   
+
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                f + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + f + "'");                  
+
+        }
+
+    }
 		
+	
+	
+	public String toString(){
+		String myString;
+		myString = myId + "," + myFirst + "," + myLast + "," + myEmail + "," + myCategory;
+		return myString;
 	}
 	
-	private void makeUser(){
-		
-	}
+	
 	
 
 }
