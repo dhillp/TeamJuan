a46d03a822c5b6cb5c79b20727e80f3092c023d3
harman <harman@d-10-16-7-91.dhcp4.washington.edu>
2017-03-09 16:21:23 -0800

    ha

--- TeamJuan/src/teamMain/EventRegistration.java
+++ TeamJuan/src/teamMain/EventRegistration.java
@@ -1,51 +1,50 @@
 package teamMain;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.FilterOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.util.ArrayList;
 
 public class EventRegistration {
 	
 	private String myFirst;
 	private String myLast;
-	private String myId;
+	private String myEvent;
 	private String myEmail;
 	private String myCategory;
-	private static ArrayList<String> list;
 
 	
-	public EventRegistration(String first, String last, String id
+	public EventRegistration(String first, String last, String event
 			,String email, String category){
 		myFirst = first;
 		myLast = last;
-		myId = id;
+		myEvent = event;
 		myEmail = email;
 		myCategory = category;
-		list = new ArrayList<String>();
-		
+		writeFile();
 	}
 	
 	private void writeFile(){
 		String st = toString();
-
-
-
+        String fileName = "Data.txt";
+        try {
+            FileWriter fileWriter =
+                new FileWriter(fileName);
+            BufferedWriter bufferedWriter =
+                new BufferedWriter(fileWriter);
+            bufferedWriter.write(toString());
+            bufferedWriter.newLine();
+			bufferedWriter.close();
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error writing to file '"
+                + fileName + "'");
+        }
     }
 
-	
-	
 	public String toString(){
 		String myString;
-		myString = myId + "," + myFirst + "," + myLast + "," + myEmail + "," + myCategory;
+		myString = myEvent + "," + myFirst + "," + myLast + "," + myEmail + "," + myCategory;
 		return myString;
 	}
 	
