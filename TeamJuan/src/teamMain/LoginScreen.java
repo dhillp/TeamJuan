d2ec15e916091b975f9d9b36e1d31069634f896e
dhillp <dhillp@uw.edu>
2017-03-09 16:49:19 -0800

    psd

--- TeamJuan/src/teamMain/LoginScreen.java
+++ TeamJuan/src/teamMain/LoginScreen.java
@@ -1,8 +1,8 @@
 package teamMain;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+//import java.awt.GridBagConstraints;
+//import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,8 +11,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+//import javax.swing.JPasswordField;
+//import javax.swing.JTextField;
+import javax.swing.JTable;
 
 public final class LoginScreen {
 	
@@ -20,17 +22,19 @@ public final class LoginScreen {
 	
 	private static final Dimension SCREEN_SIZE = KIT.getScreenSize();
 	
-    private JButton myLoginButton;
+//    private JButton myLoginButton;
 
-    private JButton myCreateButton;
+    private JButton myUpcomingButton;
     
     private JButton myJSchedButton;
     
     private JButton myRegister;
     
-    private JTextField myUsername;
+    private JTable myTable;
     
-    private JPasswordField myPassword;
+//    private JTextField myUsername;
+//    
+//    private JPasswordField myPassword;
     
     private JFrame myFrame;
     
@@ -46,54 +50,58 @@ public final class LoginScreen {
     }
     
     private void createPanel() {
-    	JPanel panel = new JPanel(new GridBagLayout());
-    	GridBagConstraints c = new GridBagConstraints();
-    	c.fill = GridBagConstraints.HORIZONTAL;
-    	myUsername = new JTextField(20);
-    	myPassword = new JPasswordField(20);
-    	JLabel user = new JLabel("Username: ");
-    	JLabel pass = new JLabel("Password: ");
-    	c.gridx = 0;
-    	c.gridy = 0;
-    	c.gridwidth = 1;
-    	panel.add(user, c);
-    	c.gridx = 1;
-    	c.gridy = 0;
-    	c.gridwidth = 2;
-    	panel.add(myUsername, c);
-    	c.gridx = 0;
-    	c.gridy = 1;
-    	c.gridwidth = 1;
-    	panel.add(pass, c);
-    	c.gridx = 1;
-    	c.gridy = 1;
-    	c.gridwidth = 2;
-    	panel.add(myPassword, c);
+//    	JPanel panel = new JPanel(new GridBagLayout());
+//    	GridBagConstraints c = new GridBagConstraints();
+//    	c.fill = GridBagConstraints.HORIZONTAL;
+//    	myUsername = new JTextField(20);
+//    	myPassword = new JPasswordField(20);
+//    	JLabel user = new JLabel("Username: ");
+//    	JLabel pass = new JLabel("Password: ");
+//    	c.gridx = 0;
+//    	c.gridy = 0;
+//    	c.gridwidth = 1;
+//    	panel.add(user, c);
+//    	c.gridx = 1;
+//    	c.gridy = 0;
+//    	c.gridwidth = 2;
+//    	panel.add(myUsername, c);
+//    	c.gridx = 0;
+//    	c.gridy = 1;
+//    	c.gridwidth = 1;
+//    	panel.add(pass, c);
+//    	c.gridx = 1;
+//    	c.gridy = 1;
+//    	c.gridwidth = 2;
+//    	panel.add(myPassword, c);
     	createButtons();
-    	JPanel panel2 = new JPanel();
-    	panel2.add(myLoginButton);
-    	panel2.add(myRegister);
-    	JPanel panel3 = new JPanel();
-    	panel3.add(myCreateButton);
-    	panel3.add(myJSchedButton);
-    	myFrame.getContentPane().add(panel3, BorderLayout.PAGE_START);
-    	myFrame.getContentPane().add(panel, BorderLayout.CENTER);
-    	myFrame.getContentPane().add(panel2, BorderLayout.PAGE_END);
+    	JPanel panel = new JPanel();
+//    	panel2.add(myLoginButton);
+    	panel.add(myRegister);
+//    	JPanel panel3 = new JPanel();
+    	panel.add(myUpcomingButton);
+    	panel.add(myJSchedButton);
+    	JLabel upcomingEvents = new JLabel("Upcoming Events: ");
+    	myFrame.getContentPane().add(upcomingEvents, BorderLayout.PAGE_START);
+    	JTable myTable = new JTable();
+    	myFrame.getContentPane().add(new JScrollPane(myTable), BorderLayout.CENTER);
+//    	myFrame.getContentPane().add(panel3, BorderLayout.PAGE_START);
+//    	myFrame.getContentPane().add(panel, BorderLayout.CENTER);
+    	myFrame.getContentPane().add(panel, BorderLayout.PAGE_END);
     }
     private void createButtons() {
-    	myLoginButton = new JButton("Login");
-    	myLoginButton.addActionListener(new ActionListener() {
+//    	myLoginButton = new JButton("Login");
+//    	myLoginButton.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				// TODO Auto-generated method stub
+//				
+//			}
+//    	});
+    	myUpcomingButton = new JButton("Upcoming Events");
+    	myUpcomingButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-    	});
-    	myCreateButton = new JButton("Create Account");
-    	myCreateButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				new CreateAccountScreen();		
+//				new CreateAccountScreen();		
 			}
     		
     	});
