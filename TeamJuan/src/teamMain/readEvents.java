5a19426b3dbededb603c6d7fc807ba9d02b2265f
harman <harman@d-10-16-7-91.dhcp4.washington.edu>
2017-03-09 17:32:23 -0800

    hs

--- TeamJuan/src/teamMain/readEvents.java
+++ TeamJuan/src/teamMain/readEvents.java
@@ -1,14 +1,29 @@
 package teamMain;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class readEvents {
 
 	readEvents(){
 		
 	}
 	
-	private Object getdata(){
+	private ArrayList<String> getdata(){
 		
-    	
-		return null;
+		ArrayList<String> eventsList = new ArrayList<String>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("events.txt")))
+        {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                eventsList.add(line);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } 
+		return eventsList;
 	}
 }
