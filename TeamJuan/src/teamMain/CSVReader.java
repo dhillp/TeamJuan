4e68e0e1387b9cd14884c1d5288d25084b4f9f20
harman <harman@d-10-16-7-91.dhcp4.washington.edu>
2017-03-09 18:25:02 -0800

    hs

--- TeamJuan/src/teamMain/CSVReader.java
+++ TeamJuan/src/teamMain/CSVReader.java
@@ -1,10 +1,5 @@
 package teamMain;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class CSVReader {
 
     public static void main(String[] args) {
